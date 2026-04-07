1dfa29fa7ba28cbf797461db16fb44d14c9b7ba6
Wilson <wilsonmhasty@gmail.com>
2026-04-07 15:01:07 -0500

    font fixed

--- Pages/index.php
+++ Pages/index.php
@@ -13,7 +13,6 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Atkinson+Hyperlegible+Next:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
-    include
 </head>
 
 <body>
