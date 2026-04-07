8b2da1cf13a28f0c6c31d065b40bc66d04c0d4f9
turingsghostiidx <coffmiest3r@gmail.com>
2026-04-07 12:06:30 -0500

    commented include file to improve readability

--- Pages/include.php
+++ Pages/include.php
@@ -1,131 +1,126 @@
 <?php
-    $headerLogo =
-    "<header>
-        <a href='index.php'>
-                <img src='ALS_Logo.jpg' alt='Arlington Life Shelter Logo'>
-        </a>
-    </header>";
+// Shared page header markup used across the site
+$headerLogo =
+"<header>
+    <a href='index.php'>
+            <img src='ALS_Logo.jpg' alt='Arlington Life Shelter Logo'>
+    </a>
+</header>";
 
-    $navLinks =
-    "<nav>
-        <ul class='links'>
+// Main navigation menu markup with dropdown and submenu structure
+$navLinks =
+"<nav>
+    <ul class='links'>
 
-            <li class='dropdown'>
-                <a href='#'>Who We Are</a>
+        <li class='dropdown'>
+            <a href='#'>Who We Are</a>
 
-                <ul class='dropdown-menu'>
-                    <li><a href='#'>Mission & History</a></li>
-                    <li><a href='#'>Frequent Questions</a></li>
-                    <li><a href='#'>Leadership & Board</a></li>
-                    <li><a href='#'>Financials</a></li>
-                    <li><a href='#'>Locations</a></li>
-                    <li><a href='#'>Job Opportunities</a></li>
-                    <li><a href='#'>Contact Us</a></li>
-                </ul>
-            </li>
+            <ul class='dropdown-menu'>
+                <li><a href='#'>Mission & History</a></li>
+                <li><a href='#'>Frequent Questions</a></li>
+                <li><a href='#'>Leadership & Board</a></li>
+                <li><a href='#'>Financials</a></li>
+                <li><a href='#'>Locations</a></li>
+                <li><a href='#'>Job Opportunities</a></li>
+                <li><a href='#'>Contact Us</a></li>
+            </ul>
+        </li>
 
-            <li class='dropdown'>
-                <a href='#'>How We Help</a>
+        <li class='dropdown'>
+            <a href='#'>How We Help</a>
 
-                <ul class='dropdown-menu'>
-                    <li><a href='#'>Programs & Services</a></li>
-                    <li><a href='#'>Results</a></li>
-                    <li><a href='#'>Entrance Requirements</a></li>
+            <ul class='dropdown-menu'>
+                <li><a href='#'>Programs & Services</a></li>
+                <li><a href='#'>Results</a></li>
+                <li><a href='#'>Entrance Requirements</a></li>
 
-                    <li class='dropdown-sub'>
-                        <a href='#'>Resources ▸</a>
+                <li class='dropdown-sub'>
+                    <a href='#'>Resources ▸</a>
 
-                        <ul class='dropdown-submenu'>
-                            <li><a href='#'>Myths About Homelessness</a></li>
-                            <li><a href='#'>Community Resources</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </li>
+                    <ul class='dropdown-submenu'>
+                        <li><a href='#'>Myths About Homelessness</a></li>
+                        <li><a href='#'>Community Resources</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
 
-            <li class='dropdown'>
-                <a href='#'>Get Involved</a>
+        <li class='dropdown'>
+            <a href='#'>Get Involved</a>
 
-                <ul class='dropdown-menu'>
-                    <li><a href='#'>Make a Gift</a></li>
-                    <li><a href='#'>Immediate Needs</a></li>
-                    <li><a href='#'>Path of Kindness - Buy a Brick</a></li>
-                    <li><a href='#'>Donation Guidelines</a></li>
+            <ul class='dropdown-menu'>
+                <li><a href='#'>Make a Gift</a></li>
+                <li><a href='#'>Immediate Needs</a></li>
+                <li><a href='#'>Path of Kindness - Buy a Brick</a></li>
+                <li><a href='#'>Donation Guidelines</a></li>
 
-                    <li class='dropdown-sub'>
-                        <a href='#'>Volunteer ▸</a>
+                <li class='dropdown-sub'>
+                    <a href='#'>Volunteer ▸</a>
 
-                        <ul class='dropdown-submenu'>
-                            <li><a href='#'>Volunteer Opportunities</a></li>
-                            <li><a href='#'>Partners of the Arlington Life Shelter (PALS)</a></li>
-                        </ul>
-                    </li>
+                    <ul class='dropdown-submenu'>
+                        <li><a href='#'>Volunteer Opportunities</a></li>
+                        <li><a href='#'>Partners of the Arlington Life Shelter (PALS)</a></li>
+                    </ul>
+                </li>
 
-                    <li class='dropdown-sub'>
-                        <a href='#'>Tour Arlington Life Shelter ▸</a>
+                <li class='dropdown-sub'>
+                    <a href='#'>Tour Arlington Life Shelter ▸</a>
 
-                        <ul class='dropdown-submenu'>
-                            <li><a href='#'>Virtual Tour</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </li>
+                    <ul class='dropdown-submenu'>
+                        <li><a href='#'>Virtual Tour</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
 
-            <li class='dropdown'>
-                <a href='#'>News & Events</a>
+        <li class='dropdown'>
+            <a href='#'>News & Events</a>
 
-                <ul class='dropdown-menu'>
-                    <li><a href='#'>Newsroom</a></li>
-                    <li><a href='#'>Join Our Mailing List</a></li>
-                    <li><a href='#'>Road Home Luncheon</a></li>
-                </ul>
-            </li>
+            <ul class='dropdown-menu'>
+                <li><a href='#'>Newsroom</a></li>
+                <li><a href='#'>Join Our Mailing List</a></li>
+                <li><a href='#'>Road Home Luncheon</a></li>
+            </ul>
+        </li>
 
-        </ul>
-    </nav>";
+    </ul>
+</nav>";
 
+// Shared page footer markup used across the site
+$footer =
+"<footer>
+    <div class='footerTop'>
+        <div class='footer1'>
+            <p>Arlington Life Shelter</p>
+            <p>325 W. Division Street</p>
+            <p>Arlington, Texas 76011</p>
+            <p>Phone (817) 548-9885</p>
 
-    
+            <a href='#'>
+                <p>Contact</p>
+            </a>
 
-    $footer =
-    "<footer>
-        <div class='footerTop'>
-            <div class='footer1'>
-                <p>Arlington Life Shelter</p>
-                <p>325 W. Division Street</p>
-                <p>Arlington, Texas 76011</p>
-                <p>Phone (817) 548-9885</p>
-                
-                <a href='#'>
-                    <p>Contact</p>
-                </a>
+            <img src='facebook.webp' alt='Facebook Icon'>
+            <img src='instagram.webp' alt='Instagram Icon'>
+            <img src='youtube.webp' alt='Youtube Icon'>
 
-                <img src='facebook.webp' alt='Facebook Icon'>
-                <img src='instagram.webp' alt='Instagram Icon'>
-                <img src='youtube.webp' alt='Youtube Icon'>
-
-                <p>Arlington Life Shelter is a 501(c)3 organization. Tax Identification 75-2235099.</p>
-            </div>
-
-
-            <div class='footer2'>
-                <img src='EIGLogo.webp' alt='Excellence in Giving Certified Transparent'>
-                <img src='CharityNav.webp' alt='Charity Navigator - Three-Star'>
-                <img src='silver.webp' alt='Silver Transparancy 2024 - Candid'>
-                <img src='Chamber.webp' alt='Greater Arlington Chamber of Commerce'>
-            </div>
+            <p>Arlington Life Shelter is a 501(c)3 organization. Tax Identification 75-2235099.</p>
         </div>
 
-
-        <hr>
-
-
-        <div class='footer3'>
-            <p>Privacy Policy</p>
-
-            <p>© 2026 Arlington Life Shelter</p>
-
-            <p>Powered by <a href='#'>Firespring</a></p>
+        <div class='footer2'>
+            <img src='EIGLogo.webp' alt='Excellence in Giving Certified Transparent'>
+            <img src='CharityNav.webp' alt='Charity Navigator - Three-Star'>
+            <img src='silver.webp' alt='Silver Transparancy 2024 - Candid'>
+            <img src='Chamber.webp' alt='Greater Arlington Chamber of Commerce'>
         </div>
-    </footer>"
+    </div>
+
+    <hr>
+
+    <div class='footer3'>
+        <p>Privacy Policy</p>
+
+        <p>© 2026 Arlington Life Shelter</p>
+    </div>
+</footer>"
 ?>
